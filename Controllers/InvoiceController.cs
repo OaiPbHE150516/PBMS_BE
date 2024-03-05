0d429efb35fc144339c8083b1ebaf0625de3d57f
BaOai.Work <oaipbhe150516@fpt.edu.vn>
2024-03-05 15:11:37 +0700

    Update InvoiceController.cs

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -26,6 +26,24 @@ namespace pbms_be.Controllers
             _mapper = mapper;
         }
 
+        // scanning invoice
+        [HttpPost("scan")]
+        public IActionResult ScanInvoice(IFormFile file)
+        {
+            if (file == null) return BadRequest("File is null or not of type pdf");
+            if (file.ContentType != ConstantConfig.MINE_TYPE_PDF
+                               && file.ContentType != ConstantConfig.MINE_TYPE_JPEG
+                               && file.ContentType != ConstantConfig.MINE_TYPE_JPG
+                               && file.ContentType != ConstantConfig.MINE_TYPE_PNG)
+                return BadRequest("File is null or not of type pdf, jpg or png");
+            var result = DocumentAiApi.ProcessDocument(file);
+            //var imageURL = GCP_BucketDA.UploadFile(file);
+            Invoice invoice = DocumentAiApi.GetInvoiceFromDocument(result);
+           // invoice.InvoiceImageURL = imageURL;
+            return Ok(invoice);
+        }
+
+
         // get all invoice by account id
         //[HttpGet("get/account/{accountID}")]
         //public IActionResult GetInvoices(string accountID)
@@ -44,28 +62,28 @@ namespace pbms_be.Controllers
         //}
 
         // get invoice by invoice id
-        [HttpGet("get/id/{invoiceID}")]
-        public IActionResult GetInvoice(int invoiceID)
-        {
-            try
-            {
-                if (invoiceID <= 0) return BadRequest("InvoiceID is required");
-                InvoiceDA invoiceDA = new InvoiceDA(_context);
-                var result = invoiceDA.GetInvoice(invoiceID);
-                return Ok(result);
-            }
-            catch (System.Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
+        //[HttpGet("get/id/{invoiceID}")]
+        //public IActionResult GetInvoice(int invoiceID)
+        //{
+        //    try
+        //    {
+        //        if (invoiceID <= 0) return BadRequest("InvoiceID is required");
+        //        InvoiceDA invoiceDA = new InvoiceDA(_context);
+        //        var result = invoiceDA.GetInvoice(invoiceID);
+        //        return Ok(result);
+        //    }
+        //    catch (System.Exception e)
+        //    {
+        //        return BadRequest(e.Message);
+        //    }
+        //}
 
-        [HttpPost("create")]
-        public IActionResult CreateInvoice([FromBody] string invoiceByteString)
-        {
-            Console.WriteLine(invoiceByteString);
-            return Ok();
-        }
+        //[HttpPost("create")]
+        //public IActionResult CreateInvoice([FromBody] string invoiceByteString)
+        //{
+        //    Console.WriteLine(invoiceByteString);
+        //    return Ok();
+        //}
 
         //[HttpPost("upload"), Route("image")]
         //public FileBundle UploadImage()
