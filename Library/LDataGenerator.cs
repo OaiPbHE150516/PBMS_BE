1f284c41916b1abcd02b4b31a52fac6f5031ce25
BaOai.Work <oaipbhe150516@fpt.edu.vn>
2024-03-07 14:50:19 +0700

    Update LDataGenerator.cs

--- Library/LDataGenerator.cs
+++ Library/LDataGenerator.cs
@@ -62,6 +62,99 @@ namespace pbms_be.Library
             return result;
         }
 
+        // generate random paragraph with random length
+        private static string GenerateRandomParagraph(int minLength, int maxLength)
+        {
+            var random = new Random();
+            var length = random.Next(minLength, maxLength);
+            var result = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(GenerateRandomString(100));
+                result.Append(" ");
+            }
+            return result.ToString();
+        }        
+        private static string GenerateRandomParagraphLorem(int length)
+        {
+            var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Pellentesque mollis neque risus, nec fermentum nisi egestas a. " +
+                "Curabitur consectetur erat quis massa fermentum pretium. " +
+                "Quisque in augue eu augue consectetur pellentesque. " +
+                "Phasellus interdum elit eleifend massa tempor porttitor. " +
+                "Morbi varius volutpat tortor, ac laoreet justo. " +
+                "Donec id leo non lectus tincidunt lacinia. " +
+                "Phasellus non purus non nisl auctor molestie." +
+                "In ut nisi in ligula dapibus tristique. " +
+                "Morbi eget enim ornare, ultricies ipsum non, semper neque. " +
+                "Suspendisse nec malesuada sem. " +
+                "Nam vel est rhoncus, tincidunt massa et, sodales lorem. " +
+                "Nunc gravida ligula vel justo mollis tincidunt posuere sed libero. " +
+                "Aenean quam arcu, elementum non viverra vitae, iaculis vitae orci. " +
+                "Nunc non turpis vitae tellus mollis suscipit vitae eu nisl. " +
+                "Nam commodo et odio et vehicula. " +
+                "Pellentesque imperdiet porta lobortis. " +
+                "Ut iaculis commodo quam ut imperdiet. " +
+                "Morbi accumsan vitae ipsum a sollicitudin. " +
+                "Quisque mollis magna est, ut mollis mauris viverra faucibus. " +
+                "Praesent vitae dolor leo. Donec mollis lectus lectus, " +
+                "et molestie quam semper eu. Ut ornare tortor non posuere vestibulum. " +
+                "Morbi at cursus dolor, vitae aliquet dolor. " +
+                "Mauris sapien turpis, efficitur in tellus et, ultricies gravida tellus. " +
+                "Suspendisse vulputate, ex feugiat porttitor fringilla, mauris felis placerat erat, " +
+                "a mollis sem elit bibendum lacus. Phasellus id congue orci. Ut ut orci ac massa " +
+                "sollicitudin lobortis ac eget lacus. Suspendisse nec nulla hendrerit, suscipit lacus vel, " +
+                "blandit nunc. Fusce placerat lacus dolor, egestas tincidunt justo pretium sit amet. " +
+                "Sed bibendum ante elit, luctus cursus leo pharetra eget. Morbi mollis a nibh vitae consequat. " +
+                "Mauris nec luctus enim, vel commodo mauris. Maecenas dictum neque turpis, ac volutpat erat congue vel. " +
+                "Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin quis vestibulum orci, " +
+                "vitae blandit quam. Suspendisse sit amet ipsum ligula. Maecenas sed pharetra ipsum. " +
+                "Vestibulum maximus consectetur turpis ultricies dignissim. " +
+                "Phasellus dictum fringilla sapien, nec sagittis eros convallis varius. " +
+                "Cras porta est non egestas blandit. Etiam quis bibendum ex. " +
+                "Morbi commodo leo eget ante porttitor sodales. Sed porttitor placerat mi, laoreet blandit arcu efficitur vitae. " +
+                "Mauris in massa arcu. Nunc ut pulvinar nulla, at tristique nibh. Donec vestibulum iaculis tortor sed fringilla. " +
+                "Etiam rhoncus massa ac vehicula finibus. Mauris ut elit pharetra, hendrerit turpis vitae, semper dolor. " +
+                "Vestibulum condimentum nibh quis lacinia malesuada. Sed risus leo, tempor quis risus sit amet, placerat luctus urna." +
+                "Aliquam suscipit interdum mauris at scelerisque. Duis pretium feugiat tortor in accumsan. " +
+                "Mauris malesuada ipsum mi, quis blandit orci efficitur eu. Vestibulum in mollis erat. " +
+                "Donec eleifend convallis finibus. Praesent nibh odio, rutrum vitae dolor non, tempor ullamcorper nulla. " +
+                "Suspendisse finibus dictum risus et posuere. Praesent ultricies molestie ligula. " +
+                "Nullam posuere, metus vel mattis pulvinar, libero felis semper tortor, ut placerat urna ligula sed est. " +
+                "Cras scelerisque nunc quis magna malesuada, ac vehicula mi hendrerit. " +
+                "Proin rutrum accumsan risus, at fermentum est luctus ac. Morbi sit amet massa placerat, tempus ex non, " +
+                "vestibulum lacus. Proin fringilla eros eget eros rhoncus sagittis.";
+            // split the string into words by space, ', ' and '. '
+            var words = lorem.Split(new string[] { " ", ", ", ". " }, StringSplitOptions.RemoveEmptyEntries);
+            var random = new Random();
+            var result = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(words[random.Next(0, words.Length)].ToLower());
+                var randomChar = random.Next(0, 3);
+                if (randomChar == 0)
+                {
+                    result.Append(" ");
+                }
+                else if (randomChar == 1)
+                {
+                    result.Append(", ");
+                }
+                else
+                {
+                    result.Append(". ");
+                }
+            }
+            // remove double space, comma, dot
+            var resultStr = result.ToString();
+            resultStr = resultStr.Replace("  ", " ");
+            resultStr = resultStr.Replace(", ,", ", ");
+            resultStr = resultStr.Replace(",,", ", ");
+            resultStr = resultStr.Replace(". .", ". ");
+            resultStr = resultStr.Replace("..", ". ");
+            return resultStr;
+        }
+
         private static string GenerateRandomDateTimeInRangeStr(DateTime min, DateTime max)
         {
             var random = new Random();
@@ -170,10 +263,10 @@ namespace pbms_be.Library
                     transaction.WalletID = wallets[random.Next(0, wallets.Count)];
                     transaction.CategoryID = categories[random.Next(0, categories.Count)];
                     transaction.TotalAmount = GenerateRandomMoneyAmountInRange(data.minAmount, data.maxAmount, data.isRoundAmount);
-                    transaction.Note = GenerateRandomString(data.minStringLength, data.maxStringLength, data.randomString);
+                    transaction.Note = GenerateRandomParagraphLorem(20);
                     transaction.TransactionDate = GenerateRandomDateTimeInRange(minDateTime, maxDateTime);
-                    transaction.FromPerson = GenerateRandomString(20);
-                    transaction.ToPerson = GenerateRandomString(25);
+                    transaction.FromPerson = GenerateRandomParagraphLorem(2);
+                    transaction.ToPerson = GenerateRandomParagraphLorem(3);
                     transaction.ImageURL = "https://storage.googleapis.com/pbms-user/collabfund/default-image.jpg";
                     transaction.ActiveStateID = ActiveStateConst.ACTIVE;
                     transactions.Add(transaction);
