8f0eced2007b1cef6eb77955f832fe411a76c940
BaOai.Work <oaipbhe150516@fpt.edu.vn>
2024-03-14 21:24:00 +0700

    Update CollabFundDTO.cs

--- DTOs/CollabFundDTO.cs
+++ DTOs/CollabFundDTO.cs
@@ -46,7 +46,6 @@ namespace pbms_be.DTOs
         public string Name { get; set; } = String.Empty;
         public string Description { get; set; } = String.Empty;
         public string ImageURL { get; set; } = String.Empty;
-        public long TotalAmount { get; set; }
     }
 
     // UpdateCollabFundDTO
