853af6dc503023900501018bacfda82db518bc58
BaOai.Work <oaipbhe150516@fpt.edu.vn>
2024-03-24 17:03:59 +0700

    Update VertextAiMultimodalApi.cs

--- ThirdParty/VertextAiMultimodalApi.cs
+++ ThirdParty/VertextAiMultimodalApi.cs
@@ -22,16 +22,9 @@ namespace pbms_be.ThirdParty
             {
                 Endpoint = $"{ConstantConfig.LOCATION}-aiplatform.googleapis.com"
             }.Build();
-
-            // Images
-            ByteString colosseum = await ReadImageFileAsync(
-                "https://storage.googleapis.com/cloud-samples-data/vertex-ai/llm/prompts/landmark1.png");
-
-            ByteString forbiddenCity = await ReadImageFileAsync(
-                "https://storage.googleapis.com/cloud-samples-data/vertex-ai/llm/prompts/landmark2.png");
-
-            ByteString christRedeemer = await ReadImageFileAsync(
-                "https://storage.googleapis.com/cloud-samples-data/vertex-ai/llm/prompts/landmark3.png");
+            // Prompt
+            string prompt = "What's in this photo";
+            string imageUri = "gs://generativeai-downloads/images/scones.jpg";
 
             // Initialize request argument(s)
             var content = new Content
@@ -40,44 +33,27 @@ namespace pbms_be.ThirdParty
             };
             content.Parts.AddRange(new List<Part>()
         {
-            new()
-            {
-                InlineData = new()
-                {
+            new() {
+                Text = prompt
+            },
+            new() {
+                FileData = new() {
                     MimeType = "image/png",
-                    Data = colosseum
-
-                }
-            },
-            new()
-            {
-                Text = "city: Rome, Landmark: the Colosseum"
-            },
-            new()
-            {
-                InlineData = new()
-                {
-                    MimeType = "image/png",
-                    Data = forbiddenCity
-                }
-            },
-            new()
-            {
-                Text = "city: Beijing, Landmark: Forbidden City"
-            },
-            new()
-            {
-                InlineData = new()
-                {
-                    MimeType = "image/png",
-                    Data = christRedeemer
+                    FileUri = imageUri
                 }
             }
         });
 
             var generateContentRequest = new GenerateContentRequest
             {
-                Model = $"projects/{projectId}/locations/{ConstantConfig.LOCATION}/publishers/{publisher}/models/{model}"
+                Model = $"projects/{projectId}/locations/{location}/publishers/{publisher}/models/{model}",
+                GenerationConfig = new GenerationConfig
+                {
+                    Temperature = 0.4f,
+                    TopP = 1,
+                    TopK = 32,
+                    MaxOutputTokens = 2048
+                }
             };
             generateContentRequest.Contents.Add(content);
 
@@ -92,15 +68,88 @@ namespace pbms_be.ThirdParty
             {
                 fullText.Append(responseItem.Candidates[0].Content.Parts[0].Text);
             }
+
             return fullText.ToString();
         }
 
-        private static async Task<ByteString> ReadImageFileAsync(string url)
-        {
-            using HttpClient client = new();
-            using var response = await client.GetAsync(url);
-            byte[] imageBytes = await response.Content.ReadAsByteArrayAsync();
-            return ByteString.CopyFrom(imageBytes);
-        }
+        //    // Images
+        //    ByteString colosseum = await ReadImageFileAsync(
+        //        "https://storage.googleapis.com/cloud-samples-data/vertex-ai/llm/prompts/landmark1.png");
+
+        //    ByteString forbiddenCity = await ReadImageFileAsync(
+        //        "https://storage.googleapis.com/cloud-samples-data/vertex-ai/llm/prompts/landmark2.png");
+
+        //    ByteString christRedeemer = await ReadImageFileAsync(
+        //        "https://storage.googleapis.com/cloud-samples-data/vertex-ai/llm/prompts/landmark3.png");
+
+        //    // Initialize request argument(s)
+        //    var content = new Content
+        //    {
+        //        Role = "USER"
+        //    };
+        //    content.Parts.AddRange(new List<Part>()
+        //{
+        //    new()
+        //    {
+        //        InlineData = new()
+        //        {
+        //            MimeType = "image/png",
+        //            Data = colosseum
+
+        //        }
+        //    },
+        //    new()
+        //    {
+        //        Text = "city: Rome, Landmark: the Colosseum"
+        //    },
+        //    new()
+        //    {
+        //        InlineData = new()
+        //        {
+        //            MimeType = "image/png",
+        //            Data = forbiddenCity
+        //        }
+        //    },
+        //    new()
+        //    {
+        //        Text = "city: Beijing, Landmark: Forbidden City"
+        //    },
+        //    new()
+        //    {
+        //        InlineData = new()
+        //        {
+        //            MimeType = "image/png",
+        //            Data = christRedeemer
+        //        }
+        //    }
+        //});
+
+        //    var generateContentRequest = new GenerateContentRequest
+        //    {
+        //        Model = $"projects/{projectId}/locations/{ConstantConfig.LOCATION}/publishers/{publisher}/models/{model}"
+        //    };
+        //    generateContentRequest.Contents.Add(content);
+
+        //    // Make the request, returning a streaming response
+        //    using PredictionServiceClient.StreamGenerateContentStream response = predictionServiceClient.StreamGenerateContent(generateContentRequest);
+
+        //    StringBuilder fullText = new();
+
+        //    // Read streaming responses from server until complete
+        //    AsyncResponseStream<GenerateContentResponse> responseStream = response.GetResponseStream();
+        //    await foreach (GenerateContentResponse responseItem in responseStream)
+        //    {
+        //        fullText.Append(responseItem.Candidates[0].Content.Parts[0].Text);
+        //    }
+        //    return fullText.ToString();
+        //}
+
+        //private static async Task<ByteString> ReadImageFileAsync(string url)
+        //{
+        //    using HttpClient client = new();
+        //    using var response = await client.GetAsync(url);
+        //    byte[] imageBytes = await response.Content.ReadAsByteArrayAsync();
+        //    return ByteString.CopyFrom(imageBytes);
+        //}
     }
 }
